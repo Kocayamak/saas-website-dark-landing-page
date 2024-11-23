0be5d03fa691e3ce5106d831405cd813a39e4ede
Kocayamak <mikailkocayamak@gmail.com>
2024-11-23 18:18:57 +0300

    navbar component added.

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,3 +1,64 @@
+import Image from "next/image";
+import MenuIcon from "../assets/icons/menu.svg";
+import logoSaas from "../assets/images/logosaas.png";
+
 export const Navbar = () => {
-  return null;
+  return (
+    <div className="bg-black">
+      <div className="px-4">
+        <div className="py-4 flex items-center justify-between">
+          <div className="relative">
+            <div className="absolute w-full top-2 bottom-0 bg-[linear-gradient(to_right,#F87BFF,#FB92CF,#FFDD9B,#C2F0B1,#2FD8FE)] blur-md"></div>
+            <Image
+              src={logoSaas}
+              alt="logo"
+              width={48}
+              height={48}
+              className="relative !h-12 !w-12"
+            />
+          </div>
+
+          <div className="border border-white border-opacity-30 h-10 w-10 inline-flex justify-center items-center rounded-lg sm:hidden">
+            <MenuIcon className="text-white" />
+          </div>
+
+          <nav className="items-center gap-6 hidden sm:flex">
+            <a
+              href="#"
+              className="text-opacity-60 text-white hover:text-opacity-100 transition"
+            >
+              Hakkımızda
+            </a>
+            <a
+              href="#"
+              className="text-opacity-60 text-white hover:text-opacity-100 transition"
+            >
+              Özellikler
+            </a>
+            <a
+              href="#"
+              className="text-opacity-60 text-white hover:text-opacity-100 transition"
+            >
+              Güncellemeler
+            </a>
+            <a
+              href="#"
+              className="text-opacity-60 text-white hover:text-opacity-100 transition"
+            >
+              Yardım
+            </a>
+            <a
+              href="#"
+              className="text-opacity-60 text-white hover:text-opacity-100 transition"
+            >
+              Müşteriler
+            </a>
+            <button className="bg-white py-2 px-4 rounded-lg">
+              Ücretsiz deneyin
+            </button>
+          </nav>
+        </div>
+      </div>
+    </div>
+  );
 };
