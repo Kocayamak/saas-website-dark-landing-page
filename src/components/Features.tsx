57a11571b4b105e5867b9f38da8282161a55352e
Kocayamak <mikailkocayamak@gmail.com>
2024-12-03 20:05:24 +0300

    features section component added.

--- src/components/Features.tsx
+++ src/components/Features.tsx
@@ -1,21 +1,55 @@
+import EcoSystemIcon from "../assets/icons/ecosystem.svg";
+
 const features = [
   {
-    title: "Integration Ecosystem",
+    title: "Entegrasyon Ekosistemi",
     description:
-      "Enhance your productivity by connecting with your tools, keeping your essentials in one place.",
+      "Araçlarınızla bağlantı kurarak ve temel ihtiyaçlarınızı tek bir yerde tutarak üretkenliğinizi artırın.",
   },
   {
-    title: "Goal Setting and Tracking",
+    title: "Hedef Belirleme ve İzleme",
     description:
-      "Define and track your goals, breaking down objectives into achievable tasks to keep your targets in sight.",
+      "Hedeflerinizi tanımlayın ve takip edin, hedeflerinizi ulaşılabilir görevlere ayırın.",
   },
   {
-    title: "Secure Data Encryption",
+    title: "Güvenli Veri Şifreleme",
     description:
-      "With end-to-end encryption, your data is securely stored and protected from unauthorized access.",
+      "Uçtan uca şifreleme ile verileriniz güvenli bir şekilde saklanır ve yetkisiz erişime karşı korunur.",
   },
 ];
 
 export const Features = () => {
-  return null;
+  return (
+    <div className="bg-black text-white py-[72px] sm:py-24">
+      <div className="container">
+        <h2 className="text-center font-bold text-5xl tracking-tighter">
+          İhtiyacınız olan her şey
+        </h2>
+
+        <div className="max-w-xl mx-auto">
+          <p className="text-center mt-5 text-xl text-white/70">
+            Tek bir yerde özelleştirilebilir listelerin, ekip çalışması
+            araçlarının ve akıllı takibin keyfini çıkarın. Görevler belirleyin,
+            hatırlatıcılar alın ve ilerlemenizi basit ve hızlı bir şekilde
+            görün.
+          </p>
+        </div>
+
+        <div className="mt-16 flex flex-col sm:flex-row gap-4">
+          {features.map(({ title, description }, index) => (
+            <div
+              key={index}
+              className="border border-white/30 px-5 py-10 text-center rounded-xl sm:flex-1"
+            >
+              <div className="inline-flex h-14 w-14 bg-white text-black justify-center items-center rounded-lg">
+                <EcoSystemIcon />
+              </div>
+              <h3 className="mt-6 font-bold ">{title}</h3>
+              <p className="mt-2 text-white/70">{description}</p>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
 };
