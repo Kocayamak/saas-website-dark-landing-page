53dd2d6344babe64cc8b237d6c1e21c890199441
Kocayamak <mikailkocayamak@gmail.com>
2024-12-06 22:18:18 +0300

    productshowcase image scroll animation added.

--- src/components/ProductShowcase.tsx
+++ src/components/ProductShowcase.tsx
@@ -1,7 +1,20 @@
+"use client";
+
 import appScreen from "../assets/images/app-screen.png";
 import Image from "next/image";
+import { motion, useScroll, useTransform } from "framer-motion";
+import { useRef } from "react";
 
 export const ProductShowcase = () => {
+  const appImageRef = useRef<HTMLImageElement>(null);
+  const { scrollYProgress } = useScroll({
+    target: appImageRef,
+    offset: ["start end", "end end"],
+  });
+
+  const rotateX = useTransform(scrollYProgress, [0, 1], [15, 0]);
+  const opacity = useTransform(scrollYProgress, [0, 1], [0.5, 1]);
+
   return (
     <div className="bg-black text-white bg-gradient-to-b from-black to-[#5D2CA8] py-[72px] sm:py-24">
       <div className="container">
@@ -17,11 +30,20 @@ export const ProductShowcase = () => {
           </p>
         </div>
 
-        <Image
-          src={appScreen}
-          alt="The product screen"
-          className="mt-14 mx-auto"
-        />
+        <motion.div
+          style={{
+            opacity,
+            rotateX,
+            transformPerspective: "800px",
+          }}
+        >
+          <Image
+            src={appScreen}
+            alt="The product screen"
+            className="mt-14 mx-auto"
+            ref={appImageRef}
+          />
+        </motion.div>
       </div>
     </div>
   );
